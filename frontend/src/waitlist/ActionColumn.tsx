ad8ad02dba9b9a359ebe2ba9ca7bccd6adf09e4e
Vu Dinh <isthis.vu@gmail.com>
2023-11-17 13:57:32 -0800

    display how long we notified customer

--- frontend/src/waitlist/ActionColumn.tsx
+++ frontend/src/waitlist/ActionColumn.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 import { IconButton } from '@mui/material';
 import SmsIcon from '@mui/icons-material/Sms';
@@ -13,6 +13,7 @@ interface ActionColumnProps {
     name: string;
     party: number;
     notified: boolean;
+    notifiedAt?: any;
     phoneNumber: string;
     msg: string;
 }
@@ -21,6 +22,10 @@ const ActionColumnWrapper = styled.div`
     display: flex;
     gap: 8px;
     align-items: center;
+    .actions {
+        display: flex;
+        flex-direction: column;
+    }
     .sms {
         color: #1976d2;
         cursor: pointer;
@@ -43,6 +48,30 @@ const ActionColumnWrapper = styled.div`
 function ActionColumn(props: ActionColumnProps) {
     const { setReloadList } = useWaitlistState();
     const { isAdmin, setDisplaySnack, setSnackMsg } = useAppState();
+    const getInitialNotifiedTimeframe = (): number => {
+        const n = Date.now();
+        const notified = new Date(props.notifiedAt);
+        const lapsed = ((n - notified.getTime()) / 1000) / 60;
+        return Math.floor(lapsed);
+    }
+
+    const [timeSinceNotified, setTimeSinceNotified] = useState(getInitialNotifiedTimeframe);
+
+    const MINUTE_MS =  60000;
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout;
+        if (props.notified) {
+            interval = setInterval(() => {
+                const n = Date.now();
+                const notified = new Date(props.notifiedAt);
+                const lapsed = ((n - notified.getTime()) / 1000) / 60;
+                setTimeSinceNotified(Math.floor(lapsed));
+                }, MINUTE_MS);
+        }
+        return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+    }, []);
+
     const notifyCustomer = () => {
         // Simple POST request with a JSON body using fetch
         const requestOptions = {
@@ -101,19 +130,26 @@ function ActionColumn(props: ActionColumnProps) {
     return (
         <ActionColumnWrapper>
             { isAdmin ? (
-                <>
-                    {renderPhoneIcons()}
-                    <IconButton
-                        onClick={(e: any) => removeCustomer()}
-                        size={'large'}
-                    >
-                        <DeleteIcon
-                            className={'delete'}
-                            fontSize={'large'}
-                        />
-                    </IconButton>
-
-                </>
+                <div className={'actions'}>
+                    <div>
+                        {renderPhoneIcons()}
+                        <IconButton
+                            onClick={(e: any) => removeCustomer()}
+                            size={'large'}
+                        >
+                            <DeleteIcon
+                                className={'delete'}
+                                fontSize={'large'}
+                            />
+                        </IconButton>
+                    </div>
+                    { props.notified ? (
+                        <div>
+                            Notified {timeSinceNotified} min ago
+                        </div>
+                    ) : <></>
+                    }
+                </div>
             ) : <></>
             }
         </ActionColumnWrapper>
